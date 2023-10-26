b6722f6b934f262fc5f32d15e6ea23d44f0b7b32
Henrique Samensari Borges <45943336+rickborges93@users.noreply.github.com>
2023-10-26 17:06:32 -0300

    UserCard api

--- githubblog/src/pages/Home/components/UserCard/index.tsx
+++ githubblog/src/pages/Home/components/UserCard/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faArrowUpRightFromSquare,
@@ -15,38 +16,62 @@ import {
   UserCardText,
   UserCardDetail,
 } from './styles'
+import { api } from '../../../../lib/axios'
+
+interface User {
+  avatar_url: string
+  html_url: string
+  name: string
+  bio: string
+  login: string
+  company: string
+  followers: number
+}
 
 export function UserCard() {
+  const [user, setUser] = useState<User>({
+    avatar_url: '',
+    html_url: '',
+    name: '',
+    bio: '',
+    login: '',
+    company: '',
+    followers: 0,
+  })
+  const username = 'rickborges93'
+
+  async function getUserInfo() {
+    const response = await api.get(`users/${username}`)
+    setUser(response.data)
+  }
+
+  useEffect(() => {
+    getUserInfo()
+  }, [])
+
   return (
     <UserCardContainer>
-      <img src="https://avatars.githubusercontent.com/u/45943336?v=4" alt="" />
+      <img src={user.avatar_url} alt="" />
       <UserCardInfos>
         <UserCardNameGithub>
-          <div>Henrique Samensari Borges</div>
-          <UserCardGithubIcon
-            href="https://github.com/rickborges93"
-            target="_blank"
-          >
+          <div>{user.name}</div>
+          <UserCardGithubIcon href={user.html_url} target="_blank">
             github
             <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
           </UserCardGithubIcon>
         </UserCardNameGithub>
-        <UserCardText>
-          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
-          pulvinar vel mass.
-        </UserCardText>
+        <UserCardText>{user.bio}</UserCardText>
         <UserCardDetails>
           <UserCardDetail>
-            <FontAwesomeIcon icon={faGithub} /> rickborges93
+            <FontAwesomeIcon icon={faGithub} /> {user.login}
           </UserCardDetail>
           <UserCardDetail>
             <FontAwesomeIcon icon={faBuilding} />
-            Infinitysys
+            {user.company}
           </UserCardDetail>
           <UserCardDetail>
             <FontAwesomeIcon icon={faUserGroup} />
-            30 seguidores
+            {user.followers} {user.followers > 1 ? 'seguidores' : 'seguidor'}
           </UserCardDetail>
         </UserCardDetails>
       </UserCardInfos>
